26fb679a0044c7c09accb065c936fbcd464e2f27
Maxim Reyman <maxir92@mail.ru>
2017-02-18 05:30:16 +0300

    Базовые критерии

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
 var link = document.querySelector(".btn-lost");
 
-	if (link) {
-		var popup = document.querySelector(".modal-content");
+if (link) {
+	var popup = document.querySelector(".modal-content");
 
-		var close = popup.querySelector(".modal-content-close");
+	var close = popup.querySelector(".modal-content-close");
 
-		var form = popup.querySelector(".login-form");
+	var form = popup.querySelector(".login-form");
 
-		var clientName = popup.querySelector(".client-name");
+	var clientName = popup.querySelector(".client-name");
 
-		var email = popup.querySelector(".client-email");
+	var email = popup.querySelector(".client-email");
 
-		var letter = popup.querySelector(".client-letter");
+	var letter = popup.querySelector(".client-letter");
 
-		var storage = localStorage.getItem(".client-name");
+	var storage = localStorage.getItem(".client-name");
 
-		link.addEventListener("click", function(event) {
-			event.preventDefault();
-			popup.classList.add("modal-content-show");
+	link.addEventListener("click", function(event) {
+		event.preventDefault();
+		popup.classList.add("modal-content-show");
 
 		if (storage) {
 			clientName.value = storage;
 			email.focus();
-		} 
-		else {
+		} else {
 			clientName.focus();
 		}
 	});
-		close.addEventListener("click", function(event) {
+	close.addEventListener("click", function(event) {
 		event.preventDefault();
 		popup.classList.remove("modal-content-show");
 		popup.classList.remove("modal-error");
@@ -38,9 +37,8 @@ var link = document.querySelector(".btn-lost");
 			event.preventDefault();
 			popup.classList.remove("modal-error");
 			popup.offsetWidth = popup.offsetWidth;
-			popup.classList.add("modal-error"); 
-		}
-		else {
+			popup.classList.add("modal-error");
+		} else {
 			localStorage.setItem(".client-name", clientName.value);
 		}
 	});
@@ -55,13 +53,13 @@ var link = document.querySelector(".btn-lost");
 }
 
 var mapOpen = document.querySelector(".js-open-map");
-	if (mapOpen) {
+if (mapOpen) {
 
-		var mapPopup = document.querySelector(".modal-content-map");
+	var mapPopup = document.querySelector(".modal-content-map");
 
-		var Close = mapPopup.querySelector(".modal-content-close");
+	var Close = mapPopup.querySelector(".modal-content-close");
 
-		mapOpen.addEventListener("click", function(event) {
+	mapOpen.addEventListener("click", function(event) {
 		event.preventDefault();
 		mapPopup.classList.add("modal-content-show");
 	});
@@ -71,16 +69,16 @@ var mapOpen = document.querySelector(".js-open-map");
 		mapPopup.classList.remove("modal-content-show");
 	});
 
-window.addEventListener("keydown", function(event) {
-	if (event.keyCode === 27) {
-		if (mapPopup.classList.contains("modal-content-show")) {
+	window.addEventListener("keydown", function(event) {
+		if (event.keyCode === 27) {
+			if (mapPopup.classList.contains("modal-content-show")) {
 				mapPopup.classList.remove("modal-content-show");
 			}
 		}
 	});
 }
 var slideopen = document.querySelector(".slider-next");
-	if (slideopen) {
+if (slideopen) {
 
 	var slideclose = document.querySelector(".slider-prev");
 
@@ -89,48 +87,47 @@ var slideopen = document.querySelector(".slider-next");
 	var slider1hide = document.querySelector(".slider-1");
 
 	slideopen.addEventListener("click", function(event) {
-		event.preventDefault(); 
-		if (slider2show.classList.contains("show-slider")) {
-		slider2show.classList.remove("show-slider");
-	} 
-	else {
-		slider2show.classList.add("show-slider");
-	}
-});
-slideopen.addEventListener("click", function(event) {
-	event.preventDefault();
-	if (slider1hide.classList.contains("slider-1-none")) {
-		slider1hide.classList.remove("slider-1-none");
-	} else {
-		slider1hide.classList.add("slider-1-none");
-	}
-});
-slideclose.addEventListener("click", function(event) {
-	event.preventDefault(); 
+		event.preventDefault();
 	if (slider2show.classList.contains("show-slider")) {
-	slider2show.classList.remove("show-slider");
-	} else {
-	slider2show.classList.add("show-slider");
-	}
-});
-slideclose.addEventListener("click", function(event) {
-	event.preventDefault();
+			slider2show.classList.remove("show-slider");
+		} else {
+			slider2show.classList.add("show-slider");
+		}
+	});
+	slideopen.addEventListener("click", function(event) {
+		event.preventDefault();
 		if (slider1hide.classList.contains("slider-1-none")) {
 			slider1hide.classList.remove("slider-1-none");
 		} else {
-		slider1hide.classList.add("slider-1-none");
+			slider1hide.classList.add("slider-1-none");
+		}
+	});
+	slideclose.addEventListener("click", function(event) {
+		event.preventDefault();
+		if (slider2show.classList.contains("show-slider")) {
+			slider2show.classList.remove("show-slider");
+		} else {
+			slider2show.classList.add("show-slider");
+		}
+	});
+	slideclose.addEventListener("click", function(event) {
+		event.preventDefault();
+		if (slider1hide.classList.contains("slider-1-none")) {
+			slider1hide.classList.remove("slider-1-none");
+		} else {
+			slider1hide.classList.add("slider-1-none");
 		}
 	});
 }
 var buy = document.querySelectorAll(".btn-buy-item");
-	if (buy) {
+if (buy) {
 
 	var contantCatalog = document.querySelector(".modal-content-catalog");
 
 	var Catalogclose = contantCatalog.querySelector(".modal-content-close");
 
-		for(var i=0;i<buy.length;i++){
-			buy[i].addEventListener("click", function(event) {
+	for (var i = 0; i < buy.length; i++) {
+		buy[i].addEventListener("click", function(event) {
 			event.preventDefault();
 			contantCatalog.classList.add("modal-content-show");
 		});
